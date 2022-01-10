df6feea406d16489595dc29cfca11ff7d2ebe47f
Thien My <myntuit@gmail.com>
2022-01-10 12:00:23 +0900

    Add binary search solution for 1631 - Path With Minimum Effort

--- data_structure/graph/problems/leetcode_1631_path_with_minimum_effort.cpp
+++ data_structure/graph/problems/leetcode_1631_path_with_minimum_effort.cpp
@@ -7,7 +7,7 @@ struct cmp {
     }
 };
 
-class Solution {
+class DijikstraSolution {
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
         int m = heights.size();
@@ -38,13 +38,53 @@ public:
     }
 };
 
+int dir[5] = {0, 1, 0, -1, 0};
+class BinarySearchSolution {
+public:
+    int m, n;
+    bool dfs(vector<vector<int>> &heights, int r, int c, int threshold, vector<vector<bool>> &visited) {
+        if (r == m-1 && c == n-1) return true;
+
+        visited[r][c] = true;
+        for (int i = 0; i < 4; ++i) {
+            int newR = r + dir[i], newC = c + dir[i+1];
+            if (newR < 0 || newR == m || newC < 0 || newC == n || visited[newR][newC]) continue;
+            if(abs(heights[r][c] - heights[newR][newC]) <= threshold &&
+               dfs(heights, newR, newC, threshold, visited)) return true;
+        }
+        return false;
+    }
+
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        m = heights.size();
+        n = heights[0].size();
+
+        int left = 0, right = 1e6;
+        int res = right;
+        while (left <= right) {
+            int mid = (right + left) / 2;
+            vector<vector<bool>> visited(m, vector<bool>(n));
+            if (dfs(heights, 0, 0, mid, visited)) {
+                res = mid;
+                right = mid-1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return res;
+    }
+};
+
 int main() {
     vector<vector<int>> heights;
     heights.push_back({1, 2, 2});
     heights.push_back({3, 8, 2});
     heights.push_back({5, 3, 5});
-    Solution s;
-    cout << "Minimum effort is: " << s.minimumEffortPath(heights) << endl;
+    DijikstraSolution s1;
+    BinarySearchSolution s2;
+    cout << "Dijikstra: " << s1.minimumEffortPath(heights) << endl;
+    cout << "Binary search:" << s2.minimumEffortPath(heights) << endl;
 
     return 0;
 }
